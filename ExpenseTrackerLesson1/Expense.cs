da90c325dad966e79569183b494b2da076a033c6
khoramiyahya@gmail.com <khoramiyahya@gmail.com>
2026-07-01 12:17:06 +0330

    khorami\task\AddClass With display sample

---
+++ ExpenseTrackerLesson1/Expense.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExpenseTrackerLesson1
+{
+    public class Expense
+    {
+        public string Title { get; set; }=string.Empty;
+        public decimal Amount { get; set; }
+        public DateTime Date { get; set; }
+        public bool IsBusiness { get; set; }
+        public int CategoryId { get; set; }
+
+    }
+}
--- ExpenseTrackerLesson1/Program.cs
+++ ExpenseTrackerLesson1/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ExpenseTrackerLesson1;
+
+Expense myExpense = new Expense() {
+    Title= "Coffee",
+    IsBusiness=false,
+    Amount = 5.6m,
+    CategoryId=1,
+    Date=DateTime.Now,
+};
+
+Console.WriteLine("Exspense Title: " + myExpense.Title);
+Console.WriteLine("Exspense Amount: " +  myExpense.Amount);
+Console.WriteLine("Exspense Category Id: " + myExpense.CategoryId);
+Console.WriteLine("Is Business Exspense: " + myExpense.IsBusiness);
+Console.WriteLine("Exspense Tax : " + myExpense.Amount * 0.15m );
+Console.WriteLine("Exspense Total With Tax : " + myExpense.Amount * 1.15m);
