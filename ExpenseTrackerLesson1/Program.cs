ae851e83007ccdb19fae834a3a74ae57f0fd47b6
khoramiyahya@gmail.com <khoramiyahya@gmail.com>
2026-07-01 12:55:28 +0330

    Khorami\Performance

--- ExpenseTrackerLesson1/Program.cs
+++ ExpenseTrackerLesson1/Program.cs
@@ -5,12 +5,17 @@ Expense myExpense = new Expense() {
     IsBusiness=false,
     Amount = 5.6m,
     CategoryId=1,
-    Date=DateTime.Now,
+    Date=new DateTime(2026,06,01),
 };
 
-Console.WriteLine("Exspense Title: " + myExpense.Title);
-Console.WriteLine("Exspense Amount: " +  myExpense.Amount);
-Console.WriteLine("Exspense Category Id: " + myExpense.CategoryId);
-Console.WriteLine("Is Business Exspense: " + myExpense.IsBusiness);
-Console.WriteLine("Exspense Tax : " + myExpense.Amount * 0.15m );
-Console.WriteLine("Exspense Total With Tax : " + myExpense.Amount * 1.15m);
+decimal taxRate = 0.15m;
+decimal tax = myExpense.Amount * taxRate;
+decimal total = myExpense.Amount + tax;
+
+
+Console.WriteLine($"Expense Title: {myExpense.Title}");
+Console.WriteLine($"Expense Amount: {myExpense.Amount}");
+Console.WriteLine($"Expense Category Id: { myExpense.CategoryId}");
+Console.WriteLine($"Is Business Exspense: { myExpense.IsBusiness}");
+Console.WriteLine($"Expense Tax : {tax}");
+Console.WriteLine($"Expense Total With Tax : {total}");
